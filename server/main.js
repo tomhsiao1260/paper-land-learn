a45e9f2cac2e7a4621f2f0cd8e62c7fa9e365aab
Hsiao Yao <tomtogo1260@gmail.com>
2023-09-25 01:11:39 +0800

    add a www page on express server

--- server/main.js
+++ server/main.js
@@ -1,1 +1,8 @@
-console.log('Hello World')
+const express = require( 'express' )
+const path = require( 'path' )
+
+const app = express()
+app.use(express.static(path.join(__dirname, '..', 'www' )))
+
+const port = 3000
+app.listen(port, () => console.log(`Listening on port ${port}!`))
